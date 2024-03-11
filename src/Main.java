2077fe5eea732dd7bd3e63b507d304c1533f7678
REIGaruki <gyakujo@mail.ru>
2024-03-11 07:57:53 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,24 @@ public class Main {
         String secondName = "Иванов";
         String middleName = "Иванович";
         String firstName ="Иван";
-        String fullName = secondName + " " + firstName + " " + middleName;
         System.out.println(task);
-        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println("Ф.И.О. сотрудника - " + fullName(firstName, middleName, secondName));
         System.out.println(task.replace('1', '2'));
-        System.out.println("Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+        firstName = "Гадя";
+        middleName = "Петрович";
+        secondName = "Хренова";
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " +
+                fullName(firstName, middleName, secondName).toUpperCase());
         System.out.println(task.replace('1', '3'));
-        System.out.println("Данные Ф.И.О. сотрудника - " + fullName.replace('ё', 'е'));
+        firstName = "Семён";
+        middleName = "Семёнович";
+        secondName = "Горбунков";
+        System.out.println("Данные Ф.И.О. сотрудника - " +
+                fullName(firstName, middleName, secondName).replace('ё', 'е'));
+    }
+    public static String fullName(String firstName, String middleName, String secondName) {
+        String fullName;
+        fullName = secondName + " " + firstName + " " + middleName;
+        return fullName;
     }
 }
