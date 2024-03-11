cf0d87fb1e3db6b1e4d4005da3df4215d19e572e
REIGaruki <gyakujo@mail.ru>
2024-03-11 07:34:52 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String task = "Задание 1";
+        String secondName = "Иванов";
+        String middleName = "Иванович";
+        String firstName ="Иван";
+        String fullName = secondName + " " + firstName + " " + middleName;
+        System.out.println(task);
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
     }
 }
